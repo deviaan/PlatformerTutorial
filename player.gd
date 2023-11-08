aad2627b6283bafd92c7e9f70dc1c566e1f60fb9
Juan Valdivia <deviaan2@gmail.com>
2023-11-07 22:04:59 -0600

    level countdown

--- player.gd
+++ player.gd
@@ -17,7 +17,6 @@ func _physics_process(delta):
 	var input_axis = Input.get_axis("move_left", "move_right")
 	handle_acceleration(input_axis, delta)
 	apply_friction(input_axis, delta)
-	update_animations(input_axis)
 
 	var was_on_floor = is_on_floor()
 	move_and_slide()
@@ -25,6 +24,8 @@ func _physics_process(delta):
 
 	if just_left_ledge:
 		coyote_jump_timer.start()
+
+	update_animations(input_axis)
 
 
 func apply_gravity(delta):
@@ -35,7 +36,7 @@ func handle_jump():
 	if is_on_floor(): air_jump = true
 
 	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
-		if Input.is_action_just_pressed("jump"):
+		if Input.is_action_pressed("jump"):
 			velocity.y = movement_data.jump_velocity
 	elif not is_on_floor():
 		if Input.is_action_just_released("jump") and velocity.y < movement_data.jump_velocity / 2:
--- world.gd
+++ world.gd
@@ -4,10 +4,20 @@ extends Node2D
 
 
 @onready var level_completed = $CanvasLayer/LevelCompleted
+@onready var start_in = %StartIn
+@onready var start_in_label = %StartInLabel
+@onready var animation_player = $AnimationPlayer
 
 
 func _ready():
 	Events.level_completed.connect(show_level_completed)
+	get_tree().paused = true
+	start_in.visible = true
+	await LevelTransition.fade_from_black()
+	animation_player.play("countdown")
+	await animation_player.animation_finished
+	get_tree().paused = false
+	start_in.visible = false
 
 
 func show_level_completed():
@@ -18,4 +28,3 @@ func show_level_completed():
 	await LevelTransition.fade_to_black()
 	get_tree().paused = false
 	get_tree().change_scene_to_packed(next_level)
-	LevelTransition.fade_from_black()
